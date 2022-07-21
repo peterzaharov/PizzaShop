e4dc1b257c902256817e86890aa76fc92f2b4f01
Пётр Захаров <peterzaharov@outlook.com>
2022-07-21 22:20:41 +0500

    Added and configured the ConfigurePizzaDialog.

--- PizzaShop/Program.cs
+++ PizzaShop/Program.cs
@@ -1,4 +1,5 @@
 using PizzaShop;
+using PizzaShop.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +8,7 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddHttpClient();
 builder.Services.AddSqlite<PizzaShopContext>("Data Source=pizza.db");
+builder.Services.AddScoped<OrderState>();
 
 var app = builder.Build();
 
