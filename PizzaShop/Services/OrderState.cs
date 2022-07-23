1c2a2e1a88ec214f1c393a71e89ea1f19d0b1848
Пётр Захаров <peterzaharov@outlook.com>
2022-07-23 16:26:44 +0500

    Added the ability to place an order.

--- PizzaShop/Services/OrderState.cs
+++ PizzaShop/Services/OrderState.cs
@@ -40,5 +40,10 @@ namespace PizzaShop.Services
         {
             Order.Pizzas.Remove(pizza);
         }
+
+        public void ResetOrder()
+        {
+            Order = new Order();
+        }
     }
 }
